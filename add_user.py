b87ef7154e3a4eb758cd41ef2bc263edd9a863ea
Mohnish nathani <nathanimohnish@gmail.com>
2020-06-20 19:32:39 +0530

    first commit

---
+++ add_user.py
@@ -0,0 +1,73 @@
+import pyaudio
+import wave
+import os
+import pickle
+import time
+from scipy.io.wavfile import read
+from IPython.display import Audio, display, clear_output
+from pydub import AudioSegment
+import sys
+from main_functions import *
+
+def add_user():
+
+    name = sys.argv[1]
+    #Voice authentication
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 2
+    RATE = 44100
+    CHUNK = 1024
+    RECORD_SECONDS = 10
+
+    source = "./voice_database/" + name
+    dst = source + "/1.wav"	
+    sound = AudioSegment.from_file(dst)
+    sound.export(dst, format="wav")
+
+    dst = source + "/2.wav"	
+    sound = AudioSegment.from_file(dst)
+    sound.export(dst, format="wav")
+
+    dst = source + "/3.wav"	
+    sound = AudioSegment.from_file(dst)
+    sound.export(dst, format="wav")
+
+    try:
+        dest =  "./gmm_models/"
+        count = 1
+
+        for path in os.listdir(source):
+            path = os.path.join(source, path)
+
+            features = np.array([])
+
+            # reading audio files of speaker
+            (sr, audio) = read(path)
+
+            # extract 40 dimensional MFCC & delta MFCC features
+            vector   = extract_features(audio,sr)
+
+            if features.size == 0:
+                features = vector
+            else:
+                features = np.vstack((features, vector))
+
+            # when features of 3 files of speaker are concatenated, then do model training
+            if count == 3:
+                gmm = GaussianMixture(n_components = 16, max_iter = 200, covariance_type='diag',n_init = 3)
+                gmm.fit(features)
+
+                # saving the trained gaussian model
+                pickle.dump(gmm, open(dest + name + '.gmm', 'wb'))
+                print(name + ' added successfully')
+
+                features = np.asarray(())
+                count = 0
+            count = count + 1
+
+    except Exception as e:
+        print('Username already exist, try another user name.')
+        print(str(e))
+
+if __name__ == '__main__':
+    add_user()
---
+++ server.js
@@ -0,0 +1,83 @@
+var express = require('express');
+var fs = require('fs');
+
+const path = require('path');
+const { spawn } = require('child_process');
+var app = express();
+
+function runScript(){
+    return spawn('python',[
+        "-u",
+        path.join(__dirname, 'recognize.py'),
+    ]);
+}
+
+function runScriptAddUser(name){
+    return spawn('python',[
+        "-u",
+        path.join(__dirname, 'add_user.py'),
+        name
+    ]);
+}
+
+
+app.use((req, res, next)=>{
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Headers','*');
+    if(req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods','PUT,POST,GET');
+        return res.status(200).json({});
+    }
+    next();
+})
+
+
+app.post('/api', function(req, res){
+    var myFile = fs.createWriteStream("myoutput.wav");
+    console.log('rcvd post request');
+    req.pipe(myFile);
+    const subprocess = runScript();
+    subprocess.stdout.on('data', (data) => {
+        console.log(`data:${data}`);
+        var name = `${data}`;
+        res.send(name);
+    });
+    subprocess.stderr.on('data', (data) => {
+        console.log(`error:${data}`);
+    });
+    subprocess.on('close', () => {
+        console.log("Closed");
+    });
+});
+
+app.post('/upload/:id/:name', function(req, res){
+    console.log('rcvd post request');
+    console.log(req.params.id);
+    const source = "./voice_database/" + req.params.name.toString();
+    fs.mkdir(source,(e)=>{
+        console.log(e);
+    });
+    var myFile = fs.createWriteStream(source+'/'+req.params.id.toString()+'.wav');
+    req.pipe(myFile);
+    res.send('New User ' +req.params.name+ ' Added');
+});
+
+app.get('/adduser/:name',function(req,res){
+    console.log(req.body);
+    const subprocess = runScriptAddUser(req.params.name);
+    console.log('got '+req.params.name);
+    subprocess.stdout.on('data', (data) => {
+        console.log(`data:${data}`);
+        var name = `${data}`;
+        res.send(name);
+    });
+    subprocess.stderr.on('data', (data) => {
+        console.log(`error:${data}`);
+    });
+    subprocess.on('close', () => {
+        console.log("Closed");
+    });
+
+});
+
+app.listen(5000);

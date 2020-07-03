c069c67cf634f8510ab64e3e0d51958154925644
Ubuntu <ubuntu@ip-172-31-46-56.ap-south-1.compute.internal>
2020-07-03 09:45:02 +0000

    latest

--- recognize.py
+++ recognize.py
@@ -26,7 +26,7 @@ def recognize():
     sound = AudioSegment.from_file(src)
     sound = sound.set_frame_rate(8000)
     sound = sound.set_channels(1)
-    sound.export(dst, format="wav")
+    sound.export(dst, format="wav", bitrate="128k")
 
 
     modelpath = "./gmm_models2/"

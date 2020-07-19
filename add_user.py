38efa58f1595ffa6bc1b6fb8a54a0c002b79ff16
Mohnish nathani <nathanimohnish@gmail.com>
2020-07-19 23:30:29 +0530

    latest

--- add_user.py
+++ add_user.py
@@ -9,56 +9,58 @@ from pydub import AudioSegment
 import sys
 from main_functions import *
 
+
 def add_user():
 
     name = sys.argv[1]
-    #Voice authentication
+    # Voice authentication
     FORMAT = pyaudio.paInt16
     CHANNELS = 1
     RATE = 8000
     CHUNK = 1024
     RECORD_SECONDS = 1.5
 
-    source = "./voice_database/" + name
-    dst = source + "/1.wav"	
-    sound = AudioSegment.from_file(dst)
-    sound = sound.set_frame_rate(8000)
-    sound = sound.set_channels(1)
-    sound = sound.set_sample_width(2)
-    sound.export(dst, format="wav")
+    nummodels = 10
 
-    dst = source + "/2.wav"	
-    sound = AudioSegment.from_file(dst)
-    sound = sound.set_frame_rate(8000)
-    sound = sound.set_channels(1)
-    sound = sound.set_sample_width(2)
-    sound.export(dst, format="wav")
+    for i in range(1, nummodels+1, 1):
+        source = "./voice_database/" + name
+        dst = source + "/"+str(i)+".wav"
+        sound = AudioSegment.from_file(dst)
+        sound = sound.set_frame_rate(8000)
+        sound = sound.set_channels(1)
+        sound = sound.set_sample_width(2)
+        sound.export(dst, format="wav")
 
-    dst = source + "/3.wav"	
-    sound = AudioSegment.from_file(dst)
-    sound = sound.set_frame_rate(8000)
-    sound = sound.set_channels(1)
-    sound = sound.set_sample_width(2)
-    sound.export(dst, format="wav")
+    # dst = source + "/2.wav"
+    # sound = AudioSegment.from_file(dst)
+    # sound = sound.set_frame_rate(8000)
+    # sound = sound.set_channels(1)
+    # sound = sound.set_sample_width(2)
+    # sound.export(dst, format="wav")
 
-#    dst = source + "/4.wav"     
+    # dst = source + "/3.wav"
+    # sound = AudioSegment.from_file(dst)
+    # sound = sound.set_frame_rate(8000)
+    # sound = sound.set_channels(1)
+    # sound = sound.set_sample_width(2)
+    # sound.export(dst, format="wav")
+
+#    dst = source + "/4.wav"
 #    sound = AudioSegment.from_file(dst)
 #    sound = sound.set_frame_rate(8000)
 #    sound = sound.set_channels(1)
 #    sound = sound.set_sample_width(2)
 #    sound.export(dst, format="wav")
 
-#    dst = source + "/5.wav"     
+#    dst = source + "/5.wav"
 #    sound = AudioSegment.from_file(dst)
 #    sound = sound.set_frame_rate(8000)
 #    sound = sound.set_channels(1)
 #    sound = sound.set_sample_width(2)
 #    sound.export(dst, format="wav")
-
-
 
     try:
-        dest =  "./gmm_models2/"
+        dest = "./gmm_models2/"
         count = 1
 
         for path in os.listdir(source):
@@ -70,7 +72,7 @@ def add_user():
             (sr, audio) = read(path)
 
             # extract 40 dimensional MFCC & delta MFCC features
-            vector   = extract_features(audio,sr)
+            vector = extract_features(audio, sr)
 
             if features.size == 0:
                 features = vector
@@ -78,8 +80,9 @@ def add_user():
                 features = np.vstack((features, vector))
 
             # when features of 3 files of speaker are concatenated, then do model training
-            if count == 3:
-                gmm = GaussianMixture(n_components = 12, max_iter = 2000, covariance_type='spherical',n_init = 50, random_state=39)
+            if count == nummodels:
+                gmm = GaussianMixture(n_components=12, max_iter=2000,
+                                      covariance_type='spherical', n_init=50, random_state=39)
                 gmm.fit(features)
 
                 # saving the trained gaussian model
@@ -93,6 +96,7 @@ def add_user():
     except Exception as e:
         print('Username already exist, try another user name.')
         print(str(e))
+
 
 if __name__ == '__main__':
     add_user()
